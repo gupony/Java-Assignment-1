f60e664e1ed6389454d089269a74f4a659e00dc7
gupony <151219889+gupony@users.noreply.github.com>
2024-01-12 15:53:58 -0400

    Add files via upload

---
+++ Sekiguchi_Alicia__assignment1_prog.java
@@ -0,0 +1,46 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Author: Alicia Sekiguchi
+ * Date: 9/20/2023
+ * Project: Assignment 1
+ */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	//First part of assignment
+		System.out.print("\"Java Journey\"");
+	    System.out.print("\n---------");
+	    System.out.print("\n\"Every error is a \\nnew learning opportunity!\"");
+	    System.out.print("\n\t - A budding programer");
+	    System.out.print("\n---------");
+		
+	
+	 //Second part of assignment
+	   
+    byte myByte=127;
+    short myShort=32767;
+    int myInt=2147483647;
+    long myLong=92233;
+    float myFloat=9.3f;
+    char myChar= 'A';
+    boolean myBoo=false;
+    double doubleOne= 15.2;
+    double doubleTwo= 20.3;
+    
+    System.out.println("\n Here are some sample values:");
+    System.out.println("Byte:" + myByte);
+    System.out.println("Short:" + myShort);
+    System.out.println("Integer:" + myInt);
+    System.out.println("Long:" + myLong);
+    System.out.println("Float:" + myFloat);
+    System.out.println("Char:" + myChar);
+    System.out.println("Boolean:" + myBoo);
+    System.out.println("The area of a rectangle with sides 15.2 and 20.3 is " + doubleOne * doubleTwo);
+    
+	}
+}
